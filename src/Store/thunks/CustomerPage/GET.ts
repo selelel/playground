2f06919e7deac8d55c75b53866a06cd2c7006c3f
selelel <136282944+selelel@users.noreply.github.com>
2023-12-28 09:28:10 +0800

    [CHORE]: testing bugs

--- src/Store/thunks/CustomerPage/GET.ts
+++ src/Store/thunks/CustomerPage/GET.ts
@@ -2,10 +2,8 @@ import { createAsyncThunk } from "@reduxjs/toolkit";
 import { supabase } from "../../../services/Supabase";
 
 export const fetchInfo = createAsyncThunk("info/Fetch", async () => {
-  const { data, error } = await supabase
-    .from("customer_info")
-    .select()
-    .order("customer_id", { ascending: true });
+  const { data, error } = await supabase.from("customer_info").select();
+
   if (error) throw error;
   return data;
 });
--- src/Store/thunks/CustomerPage/INSERT.ts
+++ src/Store/thunks/CustomerPage/INSERT.ts
@@ -6,7 +6,7 @@ export const addUser = createAsyncThunk(
   "info/Add",
   async (insert: add_info) => {
     const { error } = await supabase.from("customer_info").insert([insert]);
-
+    console.log(insert);
     if (error) throw error;
 
     return insert;
